203129c273b8a73cc633559ce4d2c79d053a3edc
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2024-09-30 22:21:54 -0700

    update

--- server.py
+++ server.py
@@ -1,27 +1,14 @@
 from fastapi import FastAPI
-from multiprocessing import Process
-import time
-from main import main
-import asyncio
+from threading import Thread
+import os
 
 app = FastAPI()
 
-def run_forever():
-    asyncio.run(main())
-
-def start_process():
-    p = Process(target=run_forever)
-    p.start()
-    return p
-
 @app.get("/")
 async def root():
+    Thread(target=run).start()
     return {"status": "ok"}
 
-if __name__ == "__main__":
-    process = start_process()
-
-    import uvicorn
-    uvicorn.run(app)
-
-    process.join()
+def run():
+    while True:
+        os.system("python main.py")
